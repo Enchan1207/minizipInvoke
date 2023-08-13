a1c16d1814162ec25a962d338772ea0e76462454
Enchan <enchant0130@gmail.com>
2023-08-13 14:00:44 +0900

    [Update] configure minizip-ng target

--- Package.swift
+++ Package.swift
@@ -15,7 +15,28 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "minizipPackage"),
+            name: "minizipPackage",
+            dependencies: ["Minizip"]),
+        .target(
+            name: "Minizip",
+            dependencies: [],
+            path: "Sources/minizip-ng",
+            exclude: [
+                "code/test", "code/doc", "code/cmake",
+                "code/mz_strm_os_win32.c", "code/mz_os_win32.c", "code/mz_crypt_winxp.c", "code/mz_crypt_winvista.c"
+            ],
+            publicHeadersPath: "code",
+            cSettings: [
+                .define("HAVE_INTTYPES_H"),
+                .define("HAVE_PKCRYPT"),
+                .define("HAVE_STDINT_H"),
+                .define("HAVE_WZAES"),
+                .define("HAVE_ZLIB"),
+                .define("ZLIB_COMPAT")
+            ],
+            linkerSettings: [
+                .linkedLibrary("z")
+            ]),
         .testTarget(
             name: "minizipPackageTests",
             dependencies: ["minizipPackage"]),
